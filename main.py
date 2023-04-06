eca979d623445861e69d6b4640af0998805acae4
Joe Yu <j56yu@uwaterloo.ca>
2023-04-06 11:15:48 -0400

    Reformat main.pu

--- main.py
+++ main.py
@@ -6,78 +6,82 @@ from GeoSpatial import GeoSpatial
 import json
 from dotenv import load_dotenv
 
-load_dotenv()
 
-client_id = os.getenv("client_id")
-client_secret = os.getenv("client_secret")
-user_agent = os.getenv("user_agent")
-username = os.getenv("username")
-password = os.getenv('password')
+def main():
+    load_dotenv()
 
-openai.organization = os.getenv("org_id")
-openai.api_key = os.getenv("OPENAI_API_KEY")
+    client_id = os.getenv("client_id")
+    client_secret = os.getenv("client_secret")
+    user_agent = os.getenv("user_agent")
+    username = os.getenv("username")
+    password = os.getenv('password')
 
-my_local_zip = int(os.getenv("my_local_zip", 10001))
-max_allowed_local_distance = float(os.getenv("max_allowed_local_distance", -1)) * 1.609344
+    openai.organization = os.getenv("org_id")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-my_location = GeoSpatial(my_local_zip)
+    my_local_zip = int(os.getenv("my_local_zip", 10001))
+    max_allowed_local_distance = float(os.getenv("max_allowed_local_distance", -1)) * 1.609344
 
-reddit = praw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     user_agent=user_agent,
-                     username=username,
-                     password=password)
+    my_location = GeoSpatial(my_local_zip)
 
-with open('API.txt', 'r') as f:
-    API_text = f.read()
+    reddit = praw.Reddit(client_id=client_id,
+                         client_secret=client_secret,
+                         user_agent=user_agent,
+                         username=username,
+                         password=password)
 
+    with open('API.txt', 'r') as f:
+        API_text = f.read()
 
-def GPT_API(submission):
-    for i in range(5):
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are an API server."},
-                    {"role": "user", "content": API_text},
-                    {"role": "assistant", "content": "ready"},
-                    {"role": "user", "content": submission.title + "\n" + submission.selftext}
-                ]
-            )
+    def GPT_API(submission):
+        for i in range(5):
             try:
-                return json.loads(response['choices'][0]['message']['content'])
-            except json.decoder.JSONDecodeError:
-                print("Bad JSON, asking again")
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "You are an API server."},
+                        {"role": "user", "content": API_text},
+                        {"role": "assistant", "content": "ready"},
+                        {"role": "user", "content": submission.title + "\n" + submission.selftext}
+                    ]
+                )
+                try:
+                    return json.loads(response['choices'][0]['message']['content'])
+                except json.decoder.JSONDecodeError:
+                    print("Bad JSON, asking again")
+                    print(f"Retry {i} / 5")
+            except Exception as e:
+                print(f"OpenAI or other errors: {e}")
                 print(f"Retry {i} / 5")
+
+    while True:
+        try:  # Praw might throw errors, we want to ignore them
+            subreddit = reddit.subreddit('hardwareswap')
+            for submission in subreddit.stream.submissions(skip_existing=True,
+                                                           pause_after=0):  # REFRESH AND LOOK FOR NEW POSTS AND PROCESS THEM
+                if submission is None:
+                    continue
+                try:
+                    h = submission.title.lower().find('[h]')
+                    w = submission.title.lower().find('[w]')
+                    if w > h:
+                        want = submission.title.lower().split('[w]')[1]
+                    else:
+                        want = submission.title.lower().split('[h]')[0]
+                except:
+                    continue
+
+                if 'paypal' or 'cash' in want.lower():  # aka if it's a selling post
+                    response = GPT_API(submission)
+                    print(response)
+                    if response['US_postal_code']:
+                        print(my_location - GeoSpatial(response['US_postal_code']) <= max_allowed_local_distance)
+
+
         except Exception as e:
-            print(f"OpenAI or other errors: {e}")
-            print(f"Retry {i} / 5")
+            print(f'Reddit Error: {e}\n Continuing')
+            time.sleep(2)
 
 
-while True:
-    try:  # Praw might throw errors, we want to ignore them
-        subreddit = reddit.subreddit('hardwareswap')
-        for submission in subreddit.stream.submissions(skip_existing=True,
-                                                       pause_after=0):  # REFRESH AND LOOK FOR NEW POSTS AND PROCESS THEM
-            if submission is None:
-                continue
-            try:
-                h = submission.title.lower().find('[h]')
-                w = submission.title.lower().find('[w]')
-                if w > h:
-                    want = submission.title.lower().split('[w]')[1]
-                else:
-                    want = submission.title.lower().split('[h]')[0]
-            except:
-                continue
-
-            if 'paypal' or 'cash' in want.lower():  # aka if it's a selling post
-                response = GPT_API(submission)
-                print(response)
-                if response['US_postal_code']:
-                    print(my_location - GeoSpatial(response['US_postal_code']) <= max_allowed_local_distance)
-
-
-    except Exception as e:
-        print(f'Reddit Error: {e}\n Continuing')
-        time.sleep(2)
+if __name__ == '__main__':
+    main()
